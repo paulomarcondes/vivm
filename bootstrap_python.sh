246197c3655ac62f38f73bbddf0c0857e6a0af26
Pmarc <pmarc.debian@gmail.com>
2013-09-05 23:12:55 -0300

    Provisioning for DVCS installation.

--- bootstrap_python.sh
+++ bootstrap_python.sh
@@ -14,3 +14,7 @@ sudo aptitude install -y vim ipython ipython-notebook python-matplotlib python-n
 
 ## Install related documentation
 sudo aptitude install -y ipython-doc python-matplotlib-doc python-numpy-doc python-simpy-doc
+
+# Install your choice of DVCS:
+sudo aptitude install -y git
+#sudo aptitude install -y mercurial
